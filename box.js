64afb3edfceedd639be84403e2547efbdbdc8b88
MrSir <73583589+MrSir007@users.noreply.github.com>
2021-07-08 18:19:25 +1000

    Add files via upload

---
+++ box.js
@@ -0,0 +1,21 @@
+class Box {
+
+    constructor(x,y,width,height) {
+
+        this.body = Bodies.rectangle(x,y,width,height);
+
+        this.width = width;
+        this.height = height;
+
+        World.add(world, this.body);
+    }
+
+    display() {
+
+        var pos = this.body.position;
+        //this.body.position.x = > pos.x
+
+        rectMode(CENTER);
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
